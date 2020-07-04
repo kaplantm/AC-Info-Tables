8ef875e34453e5e53584f23d4f21a04280659f9e
Toni Kaplan <tonarie.k@gmail.com>
2020-07-04 15:41:57 -0400

    avoid some css flickers

--- mui-wrapper.tsx
+++ mui-wrapper.tsx
@@ -1,11 +1,9 @@
 import React from "react"
 import { ThemeProvider } from "@material-ui/core/styles"
 import theme from "./src/theme"
-import CssBaseline from "@material-ui/core/CssBaseline"
 
 export default ({ element }) => (
   <>
-    <CssBaseline />
     <ThemeProvider theme={theme}>{element}</ThemeProvider>
   </>
 )
